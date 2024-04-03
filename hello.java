15463a34e23f9b1c132ea3409b161d3b36ba90bf
bagashu2525 <bagasmita25@gmal.com>
2024-04-03 21:34:06 +0530

    Intro

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 public class hello{
     public static void main(String[] args){
-        System.out.println("Hello World");
-        System.out.print("Hello Asmita");
+        System.out.print("Hello World\n");
+        System.out.print("Hello this is Asmita");
+        System.out.print(". And this is my core java repository");
     }
 }
