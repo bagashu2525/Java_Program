9e236092003ddf0da00a914c3dda5166b6d76ea3
bagashu2525 <bagasmita25@gmal.com>
2024-04-08 00:13:40 +0530

    Static variable

---
+++ Static/StaticVariable.java
@@ -0,0 +1,8 @@
+/*Static variable can be used to refer the common property of all objects
+ * 
+ * Static variables gets memory only once in the class area at the time of class loading
+ */
+
+ public class StaticVariable{
+    
+ }
