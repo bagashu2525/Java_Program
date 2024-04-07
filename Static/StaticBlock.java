23938ec0dceca4c22d10dc85118ccfc10d0ae1c6
bagashu2525 <bagasmita25@gmal.com>
2024-04-08 01:56:48 +0530

    static block

---
+++ Static/StaticBlock.java
@@ -0,0 +1,6 @@
+public class StaticBlock {
+    static{System.out.println("Static block is invoked");}
+    public static void main(String[] args){
+        System.out.println("Hello world!!");
+    }
+}
