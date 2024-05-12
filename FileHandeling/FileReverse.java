c0dd40975b4e2a85b242388ce7f4183811777d96
bagashu2525 <bagasmita25@gmal.com>
2024-05-12 12:47:41 +0530

    file read, write, reverse file data.

---
+++ FileHandeling/FileReverse.java
@@ -0,0 +1,51 @@
+package FileHandeling;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+
+public class FileReverse {
+    public static String reverse(String s){
+        char ch;
+        String newch="";
+        for(int i = 0 ;i<s.length();i++){
+            ch=s.charAt(i);
+            newch = ch + newch;
+        }
+        return newch;
+    }
+    public static void main(String[] args) throws IOException{
+        //file creation
+        File f1 = new File("File1.txt");
+        //f1.createNewFile();
+
+        //file write
+        // FileWriter out = new FileWriter(f1);
+        // out.write("hello world");
+        // out.close();
+
+        //file read using scanner class
+        Scanner in = new Scanner(f1);
+        System.err.println("The file1 content is : ");
+        while(in.hasNextLine()){
+            String s = in.nextLine();
+            System.out.println(s);
+        }
+        in.close();
+
+        //reversing word by word
+        String newch="";
+        in = new Scanner(f1);
+        while(in.hasNextLine()){
+            String s = in.nextLine();
+            newch=reverse(s);
+            //newch=newch+" "+reverse(s);
+        }
+        System.out.println(newch);
+        in.close();
+
+        
+    }
+
+}
