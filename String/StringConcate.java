7b2c5a91ecf88897ba95cda2f1617f2219ed6397
bagashu2525 <bagasmita25@gmal.com>
2024-05-12 19:08:27 +0530

    String Concatenation

---
+++ String/StringConcate.java
@@ -0,0 +1,14 @@
+package String;
+import java.util.Scanner;
+public class StringConcate {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enetr a String1: ");
+        String strng1 = sc.nextLine();
+        System.out.println("Enetr a String2: ");
+        String strng2 = sc.nextLine();
+        String newConcate = strng1 + strng2;
+        
+
+    }
+}
