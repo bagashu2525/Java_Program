8c2aa5d182281006608f58cbd3e4ffa0ddbda614
bagashu2525 <bagasmita25@gmal.com>
2024-05-26 01:44:45 +0530

    Runnable Interface

--- Thread/Main.java
+++ Thread/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args){
         Thread1 t1 = new Thread1();
         Thread2 t2 = new Thread2();
+        //Runnable interface
         Thread3 ob = new Thread3();
         Thread t3 = new Thread(ob);
 
@@ -9,6 +10,7 @@ public class Main {
         t2.start();
         t3.start();
 
+        /*run(){} can be called by thread object but it will act as a normal method calling */
 
         System.out.println("Hello World");
         // t1.setPriority(10);
