0012844625e92a0b8cb4f105bcdd144a6134da17
bagashu2525 <bagasmita25@gmal.com>
2024-05-26 01:02:05 +0530

    Thread by Runnable interface

--- Thread/Main.java
+++ Thread/Main.java
@@ -2,13 +2,18 @@ public class Main {
     public static void main(String[] args){
         Thread1 t1 = new Thread1();
         Thread2 t2 = new Thread2();
+        Thread3 ob = new Thread3();
+        Thread t3 = new Thread(ob);
 
         t1.start();
         t2.start();
+        t3.start();
+
+
         System.out.println("Hello World");
-        t1.setPriority(10);
-        t2.setPriority(1);
-        System.out.println(t1.getPriority());;
-        System.out.println(t2.getPriority());;
+        // t1.setPriority(10);
+        // t2.setPriority(1);
+        // System.out.println(t1.getPriority());;
+        // System.out.println(t2.getPriority());;
     }
 }
---
+++ Thread/Thread3.java
@@ -0,0 +1,11 @@
+public class Thread3 implements Runnable{
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        for(int i=0;i<4;i++){
+            System.out.println("Thread3!!");
+        }
+    }
+    
+}
